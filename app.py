6828fcc66685a511650c1056049948535f5cc76f
sione-tenifa <johntenifa@yahoo.com>
2021-05-04 16:26:28 -0600

    first

---
+++ app.py
@@ -0,0 +1,19 @@
+"""Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "seceret"
+
+connect_db(app)
+
+
+@app.route("/")
+def root():
+    """Render homepage."""
+
+    return render_template("index.html")
